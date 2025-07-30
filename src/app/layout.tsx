56a77fe7ed1d4d129e89c84c73301d17b1b7548f
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-07-30 12:02:46 -0300

    Cria componentes cabeçalho e menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@
 import type { Metadata } from "next";
 import { Roboto, Fjalla_One } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
+import Cabecalho from "./components/Cabecalho";
+import Menu from "./components/Menu";
 
 /* Configura variáveis para as fontes */
 const roboto = Roboto({
@@ -39,17 +40,8 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={`${roboto.variable} ${fjallaOne.variable}`}>
-        <header>
-          <h1>
-            <Link href="/">PetShop</Link>
-          </h1>
-          <nav>
-            <Link href="/">Blog</Link>
-            <Link href="/produtos">Produtos</Link>
-            <Link href="/sobre">Sobre</Link>
-            <Link href="/contato">Contato</Link>
-          </nav>
-        </header>
+        <Cabecalho />
+
         <main className="limitador">{children}</main>
       </body>
     </html>
