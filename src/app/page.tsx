b421b2407f81eff755d31bbf2572525abc69ee45
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-08-01 09:41:57 -0300

    Adiciona ListaPosts e estilos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ListaPosts from "@/components/ListaPosts";
 import estilos from "./page.module.css";
 export default function Home() {
   return (
     <section className={estilos.conteudo}>
       <h2>Pet Notícias</h2>
       <p>Notícias atualizadas sobre o mundo pet</p>
+      <ListaPosts />
     </section>
   );
 }
---
+++ src/components/ListaPosts.tsx
@@ -0,0 +1,12 @@
+// src/components/ListaPosts.tsx
+import estilos from "./ListaPosts.module.css";
+export default function ListaPosts() {
+  return (
+    <div className={estilos.posts}>
+      <article>
+        <h3>Título do post...</h3>
+        <p>Subtítulo do post...</p>
+      </article>
+    </div>
+  );
+}
