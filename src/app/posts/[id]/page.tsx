d361ea56666cbc3133df530fb5fba8a570014952
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-08-04 11:21:02 -0300

    Carrega os dados do post a partir da API e exibe

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,17 +1,34 @@
+// src/app/posts/[id]/page.tsx
 import Container from "@/components/Container";
 import estilos from "./detalhe-post.module.css";
+import { Post } from "@/types/Post";
 
-//src/app/posts/[id]/page.tsx
 type DetalhePostProps = {
   params: Promise<{ id: string }>;
 };
-export default function DetalhePost({ params }: DetalhePostProps) {
+
+/* DESAFIO! Faça um novo fetch na API usando este ID e mostre no HTML abaixo os dados obtidos! */
+
+export default async function DetalhePost({ params }: DetalhePostProps) {
+  const { id } = await params;
+  console.log(id);
+
+  const resposta = await fetch(`http://localhost:2112/posts/${id}`, {
+    next: { revalidate: 0 },
+  });
+
+  if (!resposta.ok) {
+    throw new Error("Erro ao carregar o post");
+  }
+
+  const post: Post = await resposta.json();
+
   return (
     <article className={estilos.conteudo}>
-      <h2>Título...</h2>
+      <h2>{post.titulo}</h2>
       <Container>
-        <h3>Categoria...</h3>
-        <p>Descrição...</p>
+        <h3>{post.categoria}</h3>
+        <p>{post.descricao}</p>
       </Container>
     </article>
   );
