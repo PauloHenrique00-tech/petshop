c3c56a048e29b30a9eb4ce744d885796fb32ca6f
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-08-04 12:03:11 -0300

    Refatora com função buscarPostPorId

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,25 +2,33 @@
 import Container from "@/components/Container";
 import estilos from "./detalhe-post.module.css";
 import { Post } from "@/types/Post";
-import { title } from "process";
 
 type DetalhePostProps = {
   params: Promise<{ id: string }>;
 };
 
-export async function generateMetadata({ params }: DetalhePostProps) {
-  const { id } = await params;
-  console.log(id);
-
+/* A função abaixo precisa:
+- Receber o id como string
+- Executar o acesso à API usando este id e retornar o post com os dados
+- O retorno da função DEVE SER uma Promise
+- Não se esqueça de chamar/usar esta nova função dentro do generateMetada e do
+DetalhePost no lugar do código que você irá remover. */
+async function buscarPostPorId(id: string): Promise<Post> {
   const resposta = await fetch(`http://localhost:2112/posts/${id}`, {
     next: { revalidate: 0 },
   });
 
   if (!resposta.ok) {
-    throw new Error("Erro ao carregar o post" + resposta.statusText);
+    throw Error("Erro ao buscar o post: " + resposta.statusText);
   }
 
   const post: Post = await resposta.json();
+  return post;
+}
+
+export async function generateMetadata({ params }: DetalhePostProps) {
+  const { id } = await params;
+  const post = await buscarPostPorId(id);
 
   return {
     title: post.titulo + " | PetShop",
