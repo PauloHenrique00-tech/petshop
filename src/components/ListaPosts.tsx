caac8fb304c41d6a62822607344e83ab17fa91cf
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-08-04 09:34:49 -0300

    Configura Link para rota de posts

--- src/components/ListaPosts.tsx
+++ src/components/ListaPosts.tsx
@@ -1,6 +1,7 @@
 // src/components/ListaPosts.tsx
 import { Post } from "@/types/Post";
 import estilos from "./ListaPosts.module.css";
+import Link from "next/link";
 
 type ListaPostsProps = {
   posts: Post[];
@@ -10,9 +11,17 @@ export default function ListaPosts({ posts }: ListaPostsProps) {
     <div className={estilos.posts}>
       {posts.map((post) => (
         <article key={post.id}>
-          <h3>{post.titulo}</h3>
-          <p>{post.subtitulo}</p>
+          <Link href={`/posts/${post.id}`}>
+            <h3>{post.titulo}</h3>
+            <p>{post.subtitulo}</p>
+          </Link>
         </article>
+
+        // {posts.map(({ id, titulo, subtitulo }) => (
+        // <article key={id}>
+        //   <h3>{titulo}</h3>
+        //   <p>{subtitulo}</p>
+        // </article> (versão desestrurada)
       ))}
     </div>
   );
